8cd73c2b3e6f314a8f8410a071595abfa7d1a23f
marty-cz <martin.brazdil@gmail.com>
2016-12-09 10:47:52 +0100

    Introduced configurable writer

--- src/main/java/ca/jbrains/pos/PointOfSaleTerminal.java
+++ src/main/java/ca/jbrains/pos/PointOfSaleTerminal.java
@@ -3,6 +3,7 @@ package ca.jbrains.pos;
 import com.google.common.collect.ImmutableMap;
 
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class PointOfSaleTerminal {
     public static void main(String[] args) {
@@ -14,7 +15,7 @@ public class PointOfSaleTerminal {
                                         "7070529026686", Price.cents(125)
                                 )
                         ),
-                        new WriterDisplay()
+                        new WriterDisplay(new OutputStreamWriter(System.out))
                 )::onBarcode
         ).consume(new InputStreamReader(System.in));
     }
--- src/main/java/ca/jbrains/pos/WriterDisplay.java
+++ src/main/java/ca/jbrains/pos/WriterDisplay.java
@@ -1,21 +1,33 @@
 package ca.jbrains.pos;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+
 import ca.jbrains.pos.Display;
 import ca.jbrains.pos.Price;
 
 public class WriterDisplay implements Display {
-    @Override
+	
+	private final PrintWriter writer;
+	
+    public WriterDisplay(Writer writer) {
+		this.writer = new PrintWriter(writer, true);
+	}
+
+	@Override
     public void displayPrice(Price price) {
-        System.out.println(String.format("EUR %.2f", price.euro()));
+        writer.println(String.format("EUR %.2f", price.euro()));
     }
 
     @Override
     public void displayProductNotFoundMessage(String barcodeNotFound) {
-        System.out.println(String.format("Product not found for %s", barcodeNotFound));
+    	writer.println(String.format("Product not found for %s", barcodeNotFound));
     }
 
     @Override
     public void displayScannedEmptyBarcodeMessage() {
-        System.out.println("Scanning error: empty barcode");
+    	writer.println("Scanning error: empty barcode");
     }
 }
